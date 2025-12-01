fef38793814a9ff9c55a4a9326b4d58712e4f816
Mariia Rubina <friendy.m@gmail.com>
2025-12-02 00:54:52 +0100

    chore: code cleaning

--- app/src/U2Net/u2net_mask.py
+++ app/src/U2Net/u2net_mask.py
@@ -1,21 +1,16 @@
 import os
 from pathlib import Path
-from skimage import io, transform
+from skimage import transform
 import torch
 import torchvision
 from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms#, utils
-# import torch.optim as optim
 
 import numpy as np
 from PIL import Image
-import glob
 
 from .data_loader import RescaleT
-from .data_loader import ToTensor
 from .data_loader import ToTensorLab
 from .data_loader import SalObjDataset
 
@@ -44,7 +39,6 @@ def get_mask(pred,shape):
     predict = predict.squeeze()
     predict_np = predict.cpu().data.numpy() #320x320 numpy.ndarray 2D
     im = Image.fromarray(predict_np*255).convert('RGB') #PIL.Image.Image
-    # todo take directly
     imo = im.resize((shape[1],shape[0]),resample=Image.BILINEAR)#PIL.Image.Image
     pb_np = np.array(imo)#numpy.ndarray 3D
     return pb_np
@@ -83,7 +77,6 @@ def create_mask(image_orig):
         net.load_state_dict(torch.load(model_file, map_location='cpu'))
     net.eval()
     
-
     # --------- 4. inference for each image ---------
     dl_list = list(test_salobj_dataloader)
     data_test = dl_list[0]
